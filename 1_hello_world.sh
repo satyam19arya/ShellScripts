dd5f43f771746c05c9ef3049079b05369bdee0fa
Satyam <satyam19arya@gmail.com>
2023-08-11 12:52:46 +0530

    first commit

--- 1_hello_world.sh
+++ 1_hello_world.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Hello World!"
-ls
 pwd
+echo -e "\033[0;32m success message" # green
+echo -e "\033[0;31m error message" # red
+echo -e "\033[0;33m warning message" # yellow
+echo -e "\033[0;34m info message" # blue
