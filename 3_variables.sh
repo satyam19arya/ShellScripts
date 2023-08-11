d4f674e0de27a41f5591654a4b8b86abaf3d07d5
Satyam <satyam19arya@gmail.com>
2023-08-11 18:43:42 +0530

    first commit

--- 3_variables.sh
+++ 3_variables.sh
@@ -6,3 +6,12 @@ echo "My name is $name"
 echo "What's your name?"
 read name
 echo "Hello $name, nice to meet you!"
+
+read -p "Please enter your username: " username
+read -p "Please enter your password: " -s password
+echo "Your username is $username and your password is $password"
+
+# To create a read-only variable, use the readonly command.
+readonly name
+name="Satyam_Arya"
+echo "My name is $name"
--- 4_arguments.sh
+++ 4_arguments.sh
@@ -4,4 +4,7 @@ sudo apt update
 sudo apt install $1
 $1 --version
 
-Total arguments : $#
+echo $#  # Number of arguments
+echo $@  # Print All arguments
+echo $*  # Print All arguments
+echo $0  # Print Script name
